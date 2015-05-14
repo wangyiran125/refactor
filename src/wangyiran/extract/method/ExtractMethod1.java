082327905268cffcc915397fff9b09ea93cb817a
wangyiran <wangyiran125@icloud.com>
2015-05-14 11:43:26 +0800

    assign a local variable

--- src/wangyiran/extract/method/ExtractMethod1.java
+++ src/wangyiran/extract/method/ExtractMethod1.java
@@ -1,18 +1,32 @@
 package wangyiran.extract.method;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
+ *
  * Created by wyr on 2015/5/14.
  */
-public class ExtractMethod {
-
-    List<Double> _orders = new ArrayList<>();
+public class ExtractMethod1 {
     private String _name;
+    List<Double> _orders = new ArrayList<>();
     public void printOwing(double amount){
         printBanner();
+        printBanner();
+        //reassign a local variable
+        double outstanding = getOutStanding();
         printDetails(amount);
+    }
+
+    private double getOutStanding() {
+        double outstanding = 0.0;
+        Iterator<Double> e = _orders.iterator();
+        while (e.hasNext()){
+            double each = e.next();
+            outstanding += each;
+        }
+        return outstanding;
     }
 
     private void printDetails(double amount) {
