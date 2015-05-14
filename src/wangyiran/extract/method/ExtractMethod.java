8f82d0cb74c0d24de4069d9df8f5f2523a463292
wangyiran <wangyiran125@icloud.com>
2015-05-14 11:12:08 +0800

    add statement

--- src/wangyiran/extract/method/ExtractMethod.java
+++ src/wangyiran/extract/method/ExtractMethod.java
@@ -1,12 +1,24 @@
 package wangyiran.extract.method;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by wyr on 2015/5/14.
  */
 public class ExtractMethod {
     private String _name;
+    List<Double> _orders = new ArrayList<>();
     public void printOwing(double amount){
+        Iterator<Double> e = _orders.iterator();
         printBanner();
+        double outstanding = 0.0;
+        printBanner();
+        while (e.hasNext()){
+            double each = e.next();
+            outstanding += each;
+        }
         printDetails(amount);
     }
 
