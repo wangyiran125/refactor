3419e5b920d4b2cd91d9d85ade7d749c3b85f596
wangyiran <wangyiran125@icloud.com>
2015-05-14 14:26:42 +0800

    split temp variable

---
+++ src/wangyiran/extract/method/NotSplitTemporaryVariable.java
@@ -0,0 +1,20 @@
+package wangyiran.extract.method;
+
+/**
+ * Created by wyr on 2015/5/14.
+ */
+public class NotSplitTemporaryVariable {
+    private double height;
+    private double width;
+    public void dealWith(){
+       /* That they
+        are set more than once is a sign that they have more than one responsibility within
+        the method. Any variable with more than one responsibility should be replaced with
+        a temp for each responsibility. Using a temp for two different things is very
+        confusing for the reader.*/
+        double temp = 2 *(height+width);
+        System.out.println(temp);
+        temp = height * width;
+        System.out.println(temp);
+    }
+}
