086795c8f60ea86525a3d0c50f52e47a4ec5434f
wangyiran <wangyiran125@icloud.com>
2015-05-14 14:05:40 +0800

    replace temp with query

---
+++ src/wangyiran/extract/method/NotReplaceTempWithQuery.java
@@ -0,0 +1,18 @@
+package wangyiran.extract.method;
+
+/**
+ * Created by wyr on 2015/5/14.
+ */
+public class NotReplaceTempWithQuery {
+    private double itemPrice;
+    private int quantity;
+    public double price(){
+        //temp tend to encourage longer method,because that's the only way you can reach the temp
+        double basePrice = quantity * itemPrice;
+        if (basePrice >1000){
+            return basePrice *0.95;
+        }else
+            return basePrice * 0.98;
+    }
+
+}
---
+++ src/wangyiran/extract/method/ReplaceTempWithQuery.java
@@ -0,0 +1,22 @@
+package wangyiran.extract.method;
+
+/**
+ * Created by wyr on 2015/5/14.
+ */
+public class ReplaceTempWithQuery {
+    private double itemPrice;
+    private int quantity;
+    public double price(){
+        //extract expression to a method
+        //through replace the temp with a query method,any method in the class can get at the information
+        if (basePrice() >1000){
+            return basePrice() *0.95;
+        }else
+            return basePrice() * 0.98;
+    }
+
+    private double basePrice() {
+        return quantity * itemPrice;
+    }
+
+}
