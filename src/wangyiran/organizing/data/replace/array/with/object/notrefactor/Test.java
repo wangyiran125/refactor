46b736100e395a6ef1f31181922a4af2d560c30d
wangyiran <wangyiran125@icloud.com>
2015-05-18 16:35:16 +0800

    self encapsulation

---
+++ src/wangyiran/organizing/data/replace/array/with/object/notrefactor/Test.java
@@ -0,0 +1,17 @@
+package wangyiran.organizing.data.replace.array.with.object.notrefactor;
+
+/**
+ * Created by wyr on 2015/5/18.
+ */
+public class Test {
+/*    Conventions such as ¡°the
+    first element on the array is the person¡¯s name¡± are hard to remember. With an
+    object you can use names of fields and methods to convey this information so you
+    don¡¯t have to remember it or hope the comments are up to date*/
+    String[] row = new String[3];
+    public void configure(){
+        row[0] = "liverpool";
+        row[1] = "15";
+    }
+
+}
---
+++ src/wangyiran/organizing/data/replace/array/with/object/refactor/Performance.java
@@ -0,0 +1,16 @@
+package wangyiran.organizing.data.replace.array.with.object.refactor;
+
+/**
+ * Created by wyr on 2015/5/18.
+ */
+public class Performance {
+    private String _name;
+
+    public String get_name() {
+        return _name;
+    }
+
+    public void set_name(String _name) {
+        this._name = _name;
+    }
+}
